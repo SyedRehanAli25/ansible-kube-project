4a4ab03d268ed804e1e8ebee3a440c31410d4f2d
SyedRehanAli25 <rehan.ali9325@gmail.com>
2025-07-25 22:23:53 +0530

    Update notifySlack.groovy

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -1,14 +1,9 @@
-// vars/notifySlack.groovy
-
 def call(String message, String webhookUrl) {
     def payload = [text: message]
     def jsonPayload = new groovy.json.JsonBuilder(payload).toString()
 
-    httpRequest(
-        httpMode: 'POST',
-        url: webhookUrl,
-        contentType: 'APPLICATION_JSON',
-        requestBody: jsonPayload
-    )
+    httpRequest httpMode: 'POST',
+                contentType: 'APPLICATION_JSON',
+                requestBody: jsonPayload,
+                url: webhookUrl
 }
-
