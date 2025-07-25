d0aac3bc372df0efd5a29dc474a66bfdb0fe6810
SyedRehanAli25 <rehan.ali9325@gmail.com>
2025-07-25 22:57:32 +0530

    Update notifySlack.groovy

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -1,18 +1,6 @@
-def call(String message, String webhookUrl, String channel = null) {
-    def payload = [
-        text: message
-    ]
-    if (channel) {
-        payload.channel = channel
-    }
-
-    def jsonPayload = groovy.json.JsonOutput.toJson(payload)
-
-    httpRequest(
-        httpMode: 'POST',
-        contentType: 'APPLICATION_JSON',
-        requestBody: jsonPayload,
-        url: webhookUrl,
-        validResponseCodes: '200'
-    )
+def call(String message, String webhookUrl) {
+    sh """
+        curl -X POST -H 'Content-type: application/json' \
+        --data '{"text": "${message}"}' ${webhookUrl}
+    """
 }
